f908add3a1213ed2c721e465518cb33585acd012
Tewodros Girma <TeGirma@excellerentsolutions.com>
2021-07-27 23:17:56 +0300

    api completion

---
+++ client/src/components/topbar/Topbar.jsx
@@ -0,0 +1,18 @@
+import './topbar.scss'
+
+const Topbar = () => {
+    return (
+        <div className="topbar">
+            <div className="topbarWrapper">
+                <div className="topLeft">
+                   <span className="logo">Teddyadmin</span>
+                </div>
+                <div className="topRight">
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Topbar
